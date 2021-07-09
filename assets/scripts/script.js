ce064a2f510da39790d5df059b5af1a01e6e0b8d
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-07-09 16:18:29 -0500

    Added a header to the page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,35 @@
 // ---------------- Functions ----------------
 // Run on web page load
 function init() {
+    renderHeader();
+}
+
+// Create the header and navigation on the page
+function renderHeader() {
+    let $headerNavBarDiv = $("<div>");
+    $headerNavBarDiv.addClass("navbar-fixed");
+
+    let $navBar = $("<nav>");
+    $navBar.addClass("navbar white");
+
+    let $navBarWrap = $("<div>");
+    $navBarWrap.addClass("nav-wrapper");
+
+    let $navBarTitle = $("<span>");
+    $navBarTitle.text("About Me");
+    $navBarTitle.addClass("brand-logo grey-text text-darken-4");
+
+    // <a href="#!" class="brand-logo grey-text text-darken-4">Fixed Chart</a>
+
+    // Append header to the web page
+    $navBarWrap.append($navBarTitle);
+    $navBar.append($navBarWrap);
+    $headerNavBarDiv.append($navBar);
+
+    $("header").append($headerNavBarDiv);
+
+    // <a href="#!" class="brand-logo grey-text text-darken-4">Home</a>
+
 
 }
 
