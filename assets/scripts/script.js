9f485eb6ac58a817e247f0f078b5735ef3624d9b
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-07-11 22:31:18 -0500

    Called Github with API to get pinned repositories

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -155,7 +155,7 @@ function renderHeader() {
     let $sideNavBarNetworkAccHead = $("<a>");
     $sideNavBarNetworkAccHead.addClass("collapsible-header");
     $sideNavBarNetworkAccHead.addClass("waves-effect");
-    $sideNavBarNetworkAccHead.text("Networks");
+    $sideNavBarNetworkAccHead.text("Network");
 
     let $sideNavBarNetworkAccHeadI = $("<i>");
     $sideNavBarNetworkAccHeadI.addClass("material-icons left chevron");
@@ -271,98 +271,166 @@ function renderHeader() {
     $sideNavBarUL.append($sideNavBarContactLI);
 
     $("header").append($sideNavBarUL);
-
-
-
-
-
-    // <li><div class="user-view">
-    //   <div class="background">
-    //     <img src="images/office.jpg">
-    //   </div>
-    //   <a href="#user"><img class="circle" src="images/yuna.jpg"></a>
-    //   <a href="#name"><span class="white-text name">John Doe</span></a>
-    //   <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
-    // </div></li>
-
-
-    // <ul id="sidenav-left" class="sidenav sidenav-fixed" style="transform: translateX(0px);">
-    //     <li><a href="/pages/admin-dashboard" class="logo-container">Admin<i class="material-icons left">spa</i></a></li>
-    //     <li class="no-padding">
-    //       <ul class="collapsible collapsible-accordion">
-    //         <li class="bold waves-effect"><a class="collapsible-header" tabindex="0">Pages<i class="material-icons chevron">chevron_left</i></a>
-    //           <div class="collapsible-body">
-    //             <ul>
-    //               <li><a href="/pages/admin-dashboard" class="waves-effect">Dashboard<i class="material-icons">web</i></a></li>
-    //               <li><a href="/pages/admin-fixed-chart" class="waves-effect">Fixed Chart<i class="material-icons">list</i></a></li>
-    //               <li><a href="/pages/admin-grid" class="waves-effect">Grid<i class="material-icons">dashboard</i></a></li>
-    //               <li><a href="/pages/admin-chat" class="waves-effect">Chat<i class="material-icons">chat</i></a></li>
-    //             </ul>
-    //           </div>
-    //         </li>
-    //         <li class="bold waves-effect"><a class="collapsible-header" tabindex="0">Charts<i class="material-icons chevron">chevron_left</i></a>
-    //           <div class="collapsible-body">
-    //             <ul>
-    //               <li><a href="/pages/admin-line-charts" class="waves-effect">Line Charts<i class="material-icons">show_chart</i></a></li>
-    //               <li><a href="/pages/admin-bar-charts" class="waves-effect">Bar Charts<i class="material-icons">equalizer</i></a></li>
-    //               <li><a href="/pages/admin-area-charts" class="waves-effect">Area Charts<i class="material-icons">multiline_chart</i></a></li>
-    //               <li><a href="/pages/admin-doughnut-charts" class="waves-effect">Doughnut Charts<i class="material-icons">pie_chart</i></a></li>
-    //               <li><a href="/pages/admin-financial-charts" class="waves-effect">Financial Charts<i class="material-icons">euro_symbol</i></a></li>
-    //               <li><a href="/pages/admin-interactive-charts" class="waves-effect">Interactive Charts<i class="material-icons">touch_app</i></a></li>
-    //             </ul>
-    //           </div>
-    //         </li>
-    //         <li class="bold waves-effect"><a class="collapsible-header" tabindex="0">Tables<i class="material-icons chevron">chevron_left</i></a>
-    //           <div class="collapsible-body">
-    //             <ul>
-    //               <li><a href="/pages/admin-fullscreen-table" class="waves-effect">Fullscreen with Chart<i class="material-icons">show_chart</i></a></li>
-    //               <li><a href="/pages/admin-table-custom-elements" class="waves-effect">Table with Custom Elements<i class="material-icons">settings</i></a></li>
-    //             </ul>
-    //           </div>
-    //         </li>
-    //         <li class="bold waves-effect"><a class="collapsible-header" tabindex="0">Calendar<i class="material-icons chevron">chevron_left</i></a>
-    //           <div class="collapsible-body">
-    //             <ul>
-    //               <li><a href="/pages/admin-calendar" class="waves-effect">Calendar<i class="material-icons">cloud</i></a></li>
-    //             </ul>
-    //           </div>
-    //         </li>
-    //         <li class="bold waves-effect active"><a class="collapsible-header" tabindex="0">Headers<i class="material-icons chevron">chevron_left</i></a>
-    //           <div class="collapsible-body" style="display: block;">
-    //             <ul>
-    //               <li><a href="/pages/admin-header-tabbed" class="waves-effect active">Tabbed<i class="material-icons">tab</i></a></li>
-    //               <li><a href="/pages/admin-header-metrics" class="waves-effect">Metrics<i class="material-icons">web</i></a></li>
-    //               <li><a href="/pages/admin-header-search" class="waves-effect">Search<i class="material-icons">search</i></a></li>
-    //             </ul>
-    //           </div>
-    //         </li>
-    //         <li class="bold waves-effect"><a class="collapsible-header" tabindex="0">Account<i class="material-icons chevron">chevron_left</i></a>
-    //           <div class="collapsible-body">
-    //             <ul>
-    //               <li><a href="/pages/admin-log-in" class="waves-effect">Log In<i class="material-icons">person</i></a></li>
-    //               <li><a href="/pages/admin-settings" class="waves-effect">Settings<i class="material-icons">settings</i></a></li>
-    //             </ul>
-    //           </div>
-    //         </li>
-    //       </ul>
-    //     </li>
-    //   </ul>
 }
 
 // Create the main and title
 function renderMain() {
 
+    $("main").html("");
+
     let $mainDiv = $("<div>");
-    $mainDiv.addClass("navbar-fixed");
+    $mainDiv.addClass("navbar-fixed mainContent");
 
-    let $mainDivContainer = $("<div>");
-    $mainDivContainer.addClass("container");
+    // let $mainDivContainer = $("<div>");
+    // $mainDivContainer.addClass("container");
 
-    $mainDiv.append($mainDivContainer);
+    // $mainDiv.append($mainDivContainer);
     $("main").append($mainDiv);
+}
+
+function renderHome() {
+    $("#pageTitle").text("Home");
+
+    // Clear the page content
+    $(".mainContent").html("");
+}
+
+function renderAboutMe() {
+    $("#pageTitle").text("About Me");
+
+    // Clear the page content
+    $(".mainContent").html("");
+}
+
+function renderPortfolio() {
+    $("#pageTitle").text("Portfolio");
+
+    // Clear the page content
+    $(".mainContent").html("");
+
+    // Add portfolio intro
+    let $portfolioIntro = $("<div>");
+    $portfolioIntro.text("Please take a look at my work below. Unless stated, all the work was completed by me.")
+    $(".mainContent").append($portfolioIntro);
+
+    // Create an array to store the repos
+    let dataRepos = [];
+
+    // Get Github Pinned Repos
+    let requestUrl = "https://gh-pinned-repos-5l2i19um3.vercel.app/?username=avidrunner87";
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        data.forEach(repo => {
+            // Get Github Repo Details
+            requestUrl = `https://api.github.com/repos/avidrunner87/${repo.repo}`;
+
+            fetch(requestUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                let newEntry = {
+                    "name": data.name,
+                    "created_at": data.created_at,
+                    "description": data.description,
+                    "homepage": data.homepage,
+                    "html_url": data.html_url
+                }
+                dataRepos.push(newEntry);
+            })
+        });
+
+        // Sort the repos by date created
+        dataRepos.sort((a, b) => (a.created_at > b.created_at) ? 1 : -1);
+        // dataRepos.reverse();
+        console.log(dataRepos);
+        dataRepos.forEach(repo => {
+            console.log(repo.created_at);
+        });
+
+        dataRepos.forEach(repo => {
+            let $repoDiv = $("<div>");
+            $repoDiv.addClass("row");
+
+            let $repoColCreated = $("<div>");
+            $repoColCreated.addClass("col s1 grey-text text-italic");
+            $repoColCreated.text(dayjs(data.created_at).format("YYYY MMM").toUpperCase());
+
+            $repoDiv.append($repoColCreated);
+            $(".mainContent").append($repoDiv);
+        });
+    })
+}
+
+function testGitHubAPIs() {
+    let requestUrl = "";
+
+    // Get Github Pinned Repos
+    requestUrl = "https://gh-pinned-repos-5l2i19um3.vercel.app/?username=avidrunner87";
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        
+        console.log(data);
+    })
+
+    // Get Github All Repos
+    requestUrl = "https://api.github.com/users/avidrunner87/repos";
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        
+        console.log(data);
+    })
+
+    // Get Github Repo Topics Content
+    requestUrl = "https://api.github.com/repos/avidrunner87/daily-planner/topics";
+
+    fetch(requestUrl, {
+        headers: {
+            "Accept": "application/vnd.github.mercy-preview+json"
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
 }
 
 // Calls init to retrieve data and render it to the page on load
 init();
 
 // ------------- Event Listeners -------------
+$("#sideNavLeft").click(function(event) {
+    if ($(event.target).is("a") === true || $(event.target).parent().is("a") === true) {
+        const id = event.target.id || event.target.parentNode.id;
+        if(id != "") {
+            switch (id) {
+                case "homeMe":
+                    renderHome();
+                    break;
+                case "aboutMe":
+                    renderAboutMe();
+                    break;
+                case "portfolioMe":
+                    renderPortfolio();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+})
